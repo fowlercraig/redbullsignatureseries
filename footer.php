239a2f775c0dc164572e58df1fe617a40ca8dc53
Craig Fowler <craig@wearegiants.com>
2015-06-16 12:19:48 -0700

    added tracking

--- footer.php
+++ footer.php
@@ -1,5 +1,50 @@
 <footer id="footer"></footer>
 </div><!--Wrapper-->
 <?php wp_footer(); ?>
+
+<!-- RBMH WRAPPER START -->
+<script type="text/javascript">
+
+	var edWrapperNameSpace = {};
+
+	edWrapperNameSpace.aGTM = [
+		"GTM-LVJ5", 	// hq
+		"GTM-7W7M", 	// hq
+		"GTM-HPZK", 	// hq
+		"GTM-PJQ9GM"	// property
+	];		
+	
+	var edWrapper = [];
+
+</script>
+
+<script type="text/javascript">
+
+	edWrapperNameSpace.sVers = -1; 
+	edWrapperNameSpace.aInitialInnerWrapper = [["downloads.redbull.com/webtrekk/innereWrapper/gtm/innerWrapper_gtm.js",false]];
+	edWrapperNameSpace.sWrapperSrc = ["downloads.redbull.com/webtrekk/edAnalyticsWrapper.js","downloads.redbull.com/webtrekk/confWrapper.js"];
+
+	(function (sVarName) {
+		window.edWrapperNameSpace.sEdWrapperObjectName = sVarName; window[sVarName] = window[sVarName] || [];
+		var script = document.createElement('script'); script.type = 'text/javascript'; script.asyn = false; 
+		script.src = ('https:' == document.location.protocol ? 'https://' : 'http://')+edWrapperNameSpace.sWrapperSrc[0] + "?v=" + edWrapperNameSpace.sVers;
+		var script2 = document.getElementsByTagName('script')[0]; script2.parentNode.insertBefore(script, script2);
+	})("edWrapper");
+
+</script>
+<!-- RBMH WRAPPER END -->
+
+<script type="text/javascript">
+		
+	edWrapper.push( [ 'trackPageView' , false , {1:'IntMS' , 2:'IntMS - int' , 3:'IntMS - int - redbullsignatureseries.com'} , true ] );
+
+	edWrapper.push( [ 'trackPageVar' , 'Language' , 'en'] );
+	edWrapper.push( [ 'trackPageVar' , 'Domain incl. Subdomain' , window.location.hostname] );
+	edWrapper.push( [ 'trackPageVar' , 'Type' , 'IntMS'] );
+
+	edWrapper.push(['submit']);		
+	
+</script>
+
 </body>
 </html>
